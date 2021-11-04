d771c0db439a4a1b9cfc32c8fd4a53c0fc655f74
rambkk <37767945+rambkk@users.noreply.github.com>
2021-11-04 19:50:35 +0700

    Update and rename arrayHole to arrayHole.js

--- arrayHole.js
+++ arrayHole.js
@@ -13,22 +13,22 @@
 /* Recusive function style:     */ 
 /********************************/
 
-function replaceEmptyR1(a) { return a.length?Array(0 in a?a[0]:'/*<empty>*/').concat(replaceEmpty(a.slice(1)):[]; }
-function replaceEmptyR2(a,s='/*<empty>*/') { return a.length?[0 in a?a[0]:s, ...replaceEmpty(a.slice(1))]:[]; }
+function replaceHoleR1(a) { return a.length?Array(0 in a?a[0]:'/*<empty>*/').concat(replaceEmpty(a.slice(1)):[]; }
+function replaceHoleR2(a,s='/*<empty>*/') { return a.length?[0 in a?a[0]:s, ...replaceEmpty(a.slice(1))]:[]; }
 
 
 /********************************/
 /* Iteration function style:    */
 /********************************/
-function replaceEmptyI1(a) { return Array(a.length+1).join('0').split('').map((_,k)=>k in a?a[k]:'/*<empty>*/'); }
-function replaceEmptyI2(a,s='/*<empty>*/') { return Array(a.length).fill().map((_,k)=>k in a?a[k]:s); }
-function replaceEmptyI3(a,s='/*<empty>*/') { return [...new Array(a.length)].map((_,k)=>k in a?a[k]:s); }
+function replaceHoleI1(a) { return Array(a.length+1).join('0').split('').map((_,k)=>k in a?a[k]:'/*<empty>*/'); }
+function replaceHoleI2(a,s='/*<empty>*/') { return Array(a.length).fill().map((_,k)=>k in a?a[k]:s); }
+function replaceHoleI3(a,s='/*<empty>*/') { return [...new Array(a.length)].map((_,k)=>k in a?a[k]:s); }
 
 /********************************/
 /* Classic loop function style: */
 /********************************/
 
-function replaceEmptyC1(a) {
+function replaceHoleC1(a) {
         b=a.map(v => true);
         for(var i=0;i<a.length;i++) {
                 if(!b[i]) { b[i]='/*<empty>*/';}
@@ -36,7 +36,7 @@ function replaceEmptyC1(a) {
         return b;
 }
 
-function replaceEmptyC2(a) {
+function replaceHoleC2(a) {
         var b=[];
         for(var i=0;i<a.length;i++) {
                 if(i in a) {
