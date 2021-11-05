bb36a8a1ec12d25fde00ac469f544af2c5db2561
rambkk <37767945+rambkk@users.noreply.github.com>
2021-11-05 14:30:29 +0700

    fixed bugs and added usage syntax

--- arrayHole.js
+++ arrayHole.js
@@ -2,6 +2,13 @@
  * using ideas from https://github.com/rambkk/Javascript-hole-in-array
  *
  * arrayHole (version 0.11 - initial release)
+ *
+ * Syntax:
+ *
+ *      replaceHole(ARRAY,replacement [default: undefined])
+ *
+ * The data to be used for replacement can be passed as the second parameter. 
+ * If replacement is not specified, undefined will be used.
  * 
  * Requirement: might require Javascript ES6
  * 
@@ -13,37 +20,44 @@
 /* Recusive function style:     */ 
 /********************************/
 
-function replaceHoleR1(a) { return a.length?Array(0 in a?a[0]:'/*<empty>*/').concat(replaceHole(a.slice(1)):[]; }
-function replaceHoleR2(a,s='/*<empty>*/') { return a.length?[0 in a?a[0]:s, ...replaceHole(a.slice(1))]:[]; }
+function replaceHole(a,s) { return a.length?Array(0 in a?a[0]:x).concat(replaceHole(a.slice(1),s):[]; }
+function replaceHole(a,s) { return a.length?[0 in a?a[0]:s, ...replaceHole(a.slice(1),s)]:[]; }
 
 
 /********************************/
 /* Iteration function style:    */
 /********************************/
-function replaceHoleI1(a) { return Array(a.length+1).join('0').split('').map((_,k)=>k in a?a[k]:'/*<empty>*/'); }
-function replaceHoleI2(a,s='/*<empty>*/') { return Array(a.length).fill().map((_,k)=>k in a?a[k]:s); }
-function replaceHoleI3(a,s='/*<empty>*/') { return [...new Array(a.length)].map((_,k)=>k in a?a[k]:s); }
+
+function replaceHole(a,s) { return Array(a.length+1).join('0').split('').map((_,k)=>k in a?a[k]:s); }
+function replaceHole(a,s) { return Array(a.length).fill().map((_,k)=>k in a?a[k]:s); }
+function replaceHole(a,s) { return [...new Array(a.length)].map((_,k)=>k in a?a[k]:s); }
+
 
 /********************************/
 /* Classic loop function style: */
 /********************************/
 
-function replaceHoleC1(a) {
-        b=a.map(v => true);
+function replaceHole(a,s) {
+        b=a.map(() => true);
         for(var i=0;i<a.length;i++) {
-                if(!b[i]) { b[i]='/*<empty>*/';}
+                if(b[i]) { 
+                          b[i]=a[i]; 
+                } else {
+                          b[i]=s;
+                }
         }
         return b;
 }
 
-function replaceHoleC2(a) {
+function replaceHole(a,s) {
         var b=[];
         for(var i=0;i<a.length;i++) {
                 if(i in a) {
                         b[i]=a[i];
                 } else {
-                        b[i]='/*<empty>*/';
+                        b[i]=s;
                 }
         }
         return b;
 }
+
